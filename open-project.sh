7c9d83613df3f3649e3e7cc70ca2bf583b9b26f0
exarchosgiannis <exarchosjohn9@gmail.com>
2026-06-08 10:18:41 +0200

    Ros domain = 9

--- open-project.sh
+++ open-project.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
+set -e
+
+export ROS_DOMAIN_ID=9
+export ROS_LOCALHOST_ONLY=0
+export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 
 UE_BIN="$HOME/UnrealEngine_5.7.4/Engine/Binaries/Linux/UnrealEditor"
 PROJECT="$HOME/Projects/UnrealProjects/simulator_test5.7/simulator_test57.uproject"
 
-cd "$(dirname "$UE_BIN")" || exit 1
+echo "Launching Unreal with:"
+echo "ROS_DOMAIN_ID=$ROS_DOMAIN_ID"
+echo "ROS_LOCALHOST_ONLY=$ROS_LOCALHOST_ONLY"
+echo "RMW_IMPLEMENTATION=$RMW_IMPLEMENTATION"
 
-"$UE_BIN" "$PROJECT" -VulkanNoSync
+cd "$(dirname "$UE_BIN")" || exit 1
+"$UE_BIN" "$PROJECT"
